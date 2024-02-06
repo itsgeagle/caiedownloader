62729b9e1f7e7b9c01a6268b1615a070787ead3e
Aaryan Mehta <aaryanatamd@gmail.com>
2024-02-06 12:50:32 +0530

    Bumped version to 1.0.0 in preparation of 1.0 release

--- caiedownloader.py
+++ caiedownloader.py
@@ -9,7 +9,7 @@ from modules.verification_handler import compare_version, validate_input
 from modules.popup_handler import version_popup, message_popup
 from modules.gui import *
 
-VERSION = 'v0.2.0-beta'
+VERSION = 'v1.0.0'
 
 
 # Main method for the program
