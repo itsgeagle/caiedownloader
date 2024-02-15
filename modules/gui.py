df417efd56eb0dc61972d1304ed0c693eb3302c9
Aaryan Mehta <aaryanatamd@gmail.com>
2024-02-15 23:21:12 +0530

    Fixed GUI to include the possibility to select multiple papers

--- modules/gui.py
+++ modules/gui.py
@@ -25,23 +25,23 @@ paper_type.set("Question Papers")
 title_label = Label(root, text="CAIE Downloader", font=('Montserrat', 25))
 title_label.pack()
 
-subject_label = Label(root, text="\nSubject code to download (for example, 9701)", font=('Montserrat', 16))
+subject_label = Label(root, text="\nSubject code to download (for example - 9701)", font=('Montserrat', 16))
 subject_label.pack()
 subject_input = Entry(root, textvariable=subject_var, font=('Montserrat', 16))
 subject_input.pack()
 
-paper_label = Label(root, text="\nPaper code to download (for example, 2)", font=('Montserrat', 16))
+paper_label = Label(root, text="\nPaper code(s) to download (for example - 2 or 1,2,4)", font=('Montserrat', 16))
 paper_label.pack()
 paper_input = Entry(root, textvariable=paper_var, font=('Montserrat', 16))
 paper_input.pack()
 
-start_year_label = Label(root, text="\nYear to start downloading from (for example, 2022 or 22)",
+start_year_label = Label(root, text="\nYear to start downloading from (for example - 2022 or 22)",
                          font=('Montserrat', 16))
 start_year_label.pack()
 start_year_input = Entry(root, textvariable=start_year, font=('Montserrat', 16))
 start_year_input.pack()
 
-end_year_label = Label(root, text="\nYear to end downloading from (for example, 2022 or 22)", font=('Montserrat', 16))
+end_year_label = Label(root, text="\nYear to end downloading from (for example - 2022 or 22)", font=('Montserrat', 16))
 end_year_label.pack()
 end_year_input = Entry(root, textvariable=end_year, font=('Montserrat', 16))
 end_year_input.pack()
